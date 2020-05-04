723fdf0242d7fed88f33456d9fc5d942474f2a40
kazusa_pg <kazusaprog@gmail.com>
2020-05-04 20:31:25 +0900

    Add AtCoderBeginnersSelection ABC085C

---
+++ AtCoderBeginnersSelection/ABC085C/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC085C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ny = Console.ReadLine().Split(' ')
+                .Select(i => long.Parse(i)).ToArray();
+            var n = ny[0];
+            var y = ny[1];
+            for (var i = 0; i <= n; i++)
+            {
+                for (var j = 0; j <= n - i; j++)
+                {
+                    if (i * 10000 + j * 5000 + (n - i- j) * 1000 == y)
+                    {
+                        Console.WriteLine($"{i} {j} {n - i- j}");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("-1 -1 -1");
+        }
+    }
+}
