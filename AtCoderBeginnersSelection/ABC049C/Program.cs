3babcd74d6c22d5d13521d7dfdd5805bf6f928ad
kazusa_pg <kazusaprog@gmail.com>
2020-05-07 22:53:46 +0900

    Add ABC049C

---
+++ AtCoderBeginnersSelection/ABC049C/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ABC049C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            
+            while (s != "")
+            {
+                if (s.EndsWith("dream"))
+                    s = s.Remove(s.LastIndexOf("dream", StringComparison.Ordinal), 5);
+                else if(s.EndsWith("dreamer"))
+                    s = s.Remove(s.LastIndexOf("dreamer", StringComparison.Ordinal), 7);
+                else if(s.EndsWith("erase"))
+                    s = s.Remove(s.LastIndexOf("erase", StringComparison.Ordinal), 5);
+                else if(s.EndsWith("eraser"))
+                    s = s.Remove(s.LastIndexOf("eraser", StringComparison.Ordinal), 6);
+                else
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+            }
+            Console.WriteLine("YES");
+        }
+    }
+}
